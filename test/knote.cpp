73d316fe23cbb0d791db762a06bd0f96b2ec29e9
Fred Ollinger <follinge@gmail.com>
2012-05-23 20:34:05 -0700

    Trying to add custom icon to tool bar.

--- test/knote.cpp
+++ test/knote.cpp
@@ -16,7 +16,9 @@ KNote::KNote(QWidget *parent)
 {
   qDebug() << "setting: " << componentData().componentName() + "ui.rc";
   createGUI( componentData().componentName() + "ui.rc");
-  KAction *action  = new KAction( KIcon( "delete_note" ), i18n( "Trash" ),  this );
+  KIcon *icon = new KIcon("delete_note");
+  KAction *action  = new KAction( icon, i18n( "Trash" ),  this );
+  action->setText(i18n("Trash"));
   actionCollection()->addAction( "delete_note", action );
   connect( action, SIGNAL( triggered( bool ) ), SLOT( slotDeleteNote() ) );
 }
