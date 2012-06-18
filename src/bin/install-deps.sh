dfe5d74d13012512818a047c06790d5692f262c6
Fred Ollinger <follinge@gmail.com>
2012-06-17 18:04:36 -0700

    Added more deps.

--- src/bin/install-deps.sh
+++ src/bin/install-deps.sh
@@ -1,1 +1,1 @@
-sudo apt-get install kdepim-dev g++ libboost-date-time1.42-dev kdelibs5-dev
+sudo apt-get install kdepim-dev g++ libboost-date-time1.42-dev kdelibs5-dev libboost-thread-dev libboost-filesystem1.42-dev
